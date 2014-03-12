201fafd0bc0319dc0c5d58bc63e9c6434466dd86
Jos Dirksen <jos@jpoint.nl>
2014-03-12 19:02:10 +0100

    original maze and small changes to three.js enhanced one

--- libs/maze/maze-orig.js
+++ libs/maze/maze-orig.js
@@ -1,25 +1,25 @@
-var Maze = function (scene, cells, width, height) {
-
-    this.scene = scene;
-    this.elements = [];
-
-    this.width = width;
-    this.height = height;
-
-    this.horizCells = cells;
-    this.vertCells = cells;
+var Maze = function(doc, elemId) {
+    this.canvas = doc.getElementById(elemId);
+    this.width = this.canvas.width;
+    this.height = this.canvas.height;
+    this.ctx = this.canvas.getContext('2d');
+    this.horizCells = 30;
+    this.vertCells = 30;
     this.generator = new MazeGenerator(this.horizCells, this.vertCells);
     this.cellWidth = this.width / this.horizCells;
     this.cellHeight = this.height / this.vertCells;
 
     var self = this;
 
+    self.ctx.strokeStyle = "rgb(0, 0, 0)";
+    self.ctx.fillStyle = "rgba(255, 0, 0, 0.1)";
+
     return {
-        width: function () {
+        width: function() {
             return self.width;
         },
 
-        height: function () {
+        height: function() {
             return self.height;
         },
 
@@ -27,57 +27,57 @@ var Maze = function (scene, cells, width, height) {
             self.generator.generate();
         },
 
-        draw: function () {
+        draw: function() {
             this.drawBorders();
             this.drawMaze();
         },
 
-        solve: function () {
+        solve: function() {
             self.generator.solve();
             this.drawSolution();
         },
 
-        drawBorders: function () {
+        drawBorders: function() {
             this.drawLine(self.cellWidth, 0, self.width, 0);
             this.drawLine(self.width, 0, self.width, self.height);
             this.drawLine(self.width - self.cellWidth, self.height, 0, self.height);
             this.drawLine(0, self.height, 0, 0);
         },
 
-        drawSolution: function () {
+        drawSolution: function() {
             var path = self.generator.path;
 
-            for (var i = 0; i < path.length; i++) {
+            for(var i = 0; i < path.length; i++) {
                 (function () {
                     var cell = path[i];
                     var x = cell.x * self.cellWidth;
                     var y = cell.y * self.cellHeight;
-                    setTimeout(function () {
+                    setTimeout(function() {
                         self.ctx.fillRect(x, y, self.cellWidth, self.cellHeight);
                     }, 80 * i);
                 })();
             }
         },
 
-        drawMaze: function () {
+        drawMaze: function() {
             var graph = self.generator.graph;
             var drawnEdges = [];
 
-            var edgeAlreadyDrawn = function (cell1, cell2) {
-                return _.detect(drawnEdges, function (edge) {
+            var edgeAlreadyDrawn = function(cell1, cell2) {
+                return _.detect(drawnEdges, function(edge) {
                     return _.include(edge, cell1) && _.include(edge, cell2);
                 }) != undefined;
             };
 
-            for (var i = 0; i < graph.width; i++) {
-                for (var j = 0; j < graph.height; j++) {
+            for(var i = 0; i < graph.width; i++) {
+                for(var j = 0; j < graph.height; j++) {
                     var cell = graph.cells[i][j];
                     var topCell = graph.getCellAt(cell.x, cell.y - 1);
                     var leftCell = graph.getCellAt(cell.x - 1, cell.y);
                     var rightCell = graph.getCellAt(cell.x + 1, cell.y);
                     var bottomCell = graph.getCellAt(cell.x, cell.y + 1);
 
-                    if (!edgeAlreadyDrawn(cell, topCell) && graph.areConnected(cell, topCell)) {
+                    if(!edgeAlreadyDrawn(cell, topCell) && graph.areConnected(cell, topCell)) {
                         var x1 = cell.x * self.cellWidth;
                         var y1 = cell.y * self.cellHeight;
                         var x2 = x1 + self.cellWidth;
@@ -87,7 +87,7 @@ var Maze = function (scene, cells, width, height) {
                         drawnEdges.push([cell, topCell]);
                     }
 
-                    if (!edgeAlreadyDrawn(cell, leftCell) && graph.areConnected(cell, leftCell)) {
+                    if(!edgeAlreadyDrawn(cell, leftCell) && graph.areConnected(cell, leftCell)) {
                         var x2 = x1;
                         var y2 = y1 + self.cellHeight;
 
@@ -95,7 +95,7 @@ var Maze = function (scene, cells, width, height) {
                         drawnEdges.push([cell, leftCell]);
                     }
 
-                    if (!edgeAlreadyDrawn(cell, rightCell) && graph.areConnected(cell, rightCell)) {
+                    if(!edgeAlreadyDrawn(cell, rightCell) && graph.areConnected(cell, rightCell)) {
                         var x1 = (cell.x * self.cellWidth) + self.cellWidth;
                         var y1 = cell.y * self.cellHeight;
                         var x2 = x1;
@@ -105,7 +105,7 @@ var Maze = function (scene, cells, width, height) {
                         drawnEdges.push([cell, rightCell]);
                     }
 
-                    if (!edgeAlreadyDrawn(cell, bottomCell) && graph.areConnected(cell, bottomCell)) {
+                    if(!edgeAlreadyDrawn(cell, bottomCell) && graph.areConnected(cell, bottomCell)) {
                         var x1 = cell.x * self.cellWidth;
                         var y1 = (cell.y * self.cellHeight) + self.cellHeight;
                         var x2 = x1 + self.cellWidth;
@@ -118,32 +118,11 @@ var Maze = function (scene, cells, width, height) {
             }
         },
 
-        getElements: function() {
-            return self.elements;
-        },
-
-        drawLine: function (x1, y1, x2, y2) {
-            var lengthX = Math.abs(x1 - x2);
-            var lengthY = Math.abs(y1 - y2);
-
-            // since only 90 degrees angles, so one of these is always 0
-            // to add a certain thickness to the wall, set to 0.5
-            if (lengthX === 0) lengthX = 0.5;
-            if (lengthY === 0) lengthY = 0.5;
-
-            // create a cube to represent the wall segment
-            var wallGeom = new THREE.CubeGeometry(lengthX, 3, lengthY);
-            var wallMaterial = new THREE.MeshPhongMaterial({color: 0xff0000, opacity: 0.8, transparent: true});
-
-
-            // and create the complete wall segment
-            var wallMesh = new THREE.Mesh(wallGeom, wallMaterial);
-
-            // finally position it correctly
-            wallMesh.position = new THREE.Vector3(x1 - ((x1 - x2) / 2) -(self.height/2), wallGeom.height/2, y1 - ((y1 - y2)) / 2 - (self.width /2));
-
-            self.elements.push(wallMesh);
-            scene.add(wallMesh);
+        drawLine: function(x1, y1, x2, y2) {
+            self.ctx.beginPath();
+            self.ctx.moveTo(x1, y1);
+            self.ctx.lineTo(x2, y2);
+            self.ctx.stroke();
         }
     };
 };
